c7279e2e691a4b3c276d4b4fb67d2f07127edf6f
ska <sanderkaasen@hotmail.com>
2022-10-13 18:46:02 +0200

    Init commit

---
+++ src/test/kotlin/no/dunder/mifflin/dundermifflinquotes/DunderMifflinQuotesApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.dunder.mifflin.dundermifflinquotes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DunderMifflinQuotesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
