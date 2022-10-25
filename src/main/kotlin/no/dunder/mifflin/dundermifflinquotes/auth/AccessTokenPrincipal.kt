ed79af3ea32a526859603fb4e6cfcb3bee54393f
Sander Kaasen <sander.kaasen@nrk.no>
2022-10-25 15:55:48 +0200

    Prefixed entity classes to data classes

--- src/main/kotlin/no/dunder/mifflin/dundermifflinquotes/auth/AccessTokenPrincipal.kt
+++ src/main/kotlin/no/dunder/mifflin/dundermifflinquotes/auth/AccessTokenPrincipal.kt
@@ -2,7 +2,7 @@ package no.dunder.mifflin.dundermifflinquotes.auth
 
 import java.security.Principal
 
-class AccessTokenPrincipal(private val name: String) : Principal {
+data class AccessTokenPrincipal(private val name: String) : Principal {
     override fun getName(): String {
         return name
     }
--- src/main/kotlin/no/dunder/mifflin/dundermifflinquotes/domain/dto/Quote.kt
+++ src/main/kotlin/no/dunder/mifflin/dundermifflinquotes/domain/dto/Quote.kt
@@ -1,3 +1,3 @@
 package no.dunder.mifflin.dundermifflinquotes.domain.dto
 
-class Quote(val id: Int, val season: Int, val episode: Int, val scene: Int, val lineText: String, val speaker: String, val deleted: Boolean)
+data class Quote(val id: Int, val season: Int, val episode: Int, val scene: Int, val lineText: String, val speaker: String, val deleted: Boolean)
